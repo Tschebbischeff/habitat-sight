a702c803997bcd3a27dc629d3f09bb237ac1a17e
Lontra Felina <26169724+Tschebbischeff@users.noreply.github.com>
2026-05-06 00:35:16 +0200

    Fixes yq expression for adding repository to development glance page for real

--- habitat-config/glance/config/glance.yml.sh
+++ habitat-config/glance/config/glance.yml.sh
@@ -5,6 +5,7 @@ set -euo pipefail
 TARGET_FILE="$1"
 TARGET_FILE_CONTENT="$(cat "$TARGET_FILE")"
 
+
 # ### Init
 
 SOURCE_FILE_DEV_REPOSITORIES="$(mktemp)"
@@ -15,15 +16,19 @@ cat >"$SOURCE_FILE_DEV_REPOSITORIES" <<'EOF'
 - dockerhub:bluenviron/mediamtx:latest
 EOF
 
+
 # ### Add to "Development" page
 
-# shellcheck disable=2016 # Variables are internal to yq expression
-TARGET_FILE_CONTENT="$(yq eval '
+YQ_ADD_REPOSITORIES='
   (
     .pages[] | select(.name == "'"$DEV_PAGE_NAME"'").columns[0].widgets[] |
-    select(.type == "releases") | .[0]
-  ).repositories += load("'"$SOURCE_FILE_DEV_REPOSITORIES"'")
-' <<<"$TARGET_FILE_CONTENT")"
+    select(.type == "releases") | .repositories
+  ) += load("'"$SOURCE_FILE_DEV_REPOSITORIES"'")
+'
+
+# shellcheck disable=2016 # Variables are internal to yq expression
+TARGET_FILE_CONTENT="$(yq eval "$YQ_ADD_REPOSITORIES" <<<"$TARGET_FILE_CONTENT")"
+
 
 # ### Clean Up, Print Result and Exit
 rm "$SOURCE_FILE_DEV_REPOSITORIES"
